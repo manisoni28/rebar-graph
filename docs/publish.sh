2428ac498a6e2e389e888cde39475c390523be67
Rob Schoening <robschoening@gmail.com>
2019-01-28 20:20:52 -0800

    docs

--- docs/publish.sh
+++ docs/publish.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 
-rm -rf gh-pages site
+rm -rf tmp-clone site
 
-REMOTE_URL=`git remote -v | grep push | grep origin | awk '{ print $2 }'`
+REMOTE_URL=git@github.com:rebar-cloud/rebar-cloud.git
 
-git clone -b gh-pages $REMOTE_URL gh-pages
+git clone $REMOTE_URL tmp-clone
 
 docker build . -t mkdocs
 
 docker run -v `pwd`:/docs -p 8000:8000 -it mkdocs mkdocs build
 
-cp -r site/ gh-pages
+cp -r site/ tmp-clone
 
-cd gh-pages
+cd tmp-clone
 git add .
 git commit -a -m "update"
 
