da7d47e08e4cfc10e3896fa91b4b21e6b3159f73
Rob Schoening <robschoening@gmail.com>
2019-01-28 21:08:46 -0800

    foo

--- docs/circleci-publish.sh
+++ docs/circleci-publish.sh
@@ -24,11 +24,11 @@ cd tmp-clone
 
   git config --global user.email "robschoening@gmail.com"
   git config --global user.name "Circle CI"
-  
+
 git add .
-git commit -a -m "docs built from $SOURCE_SHA1"
+git commit -a -m "docs built from $SOURCE_SHA1" || exit 99
 
-git push 
+git push || exit 99
 
-exit 0
+
 
